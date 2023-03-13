7121e6c6cfe52ef2546d90fcd8ac2cc54271ca07
Камиль <94285493+kamil19862307@users.noreply.github.com>
2023-03-13 22:50:06 +0300

    instal laravel, bootstrap

---
+++ resources/js/app.js
@@ -0,0 +1,16 @@
+import './bootstrap';
+import './bootstrap.bundle.min';
+import './custom';
+import './jquery-1.11.0.min';
+import './jquery-migrate-1.2.1.min';
+import './slick.min';
+import './templatemo';
+import './templatemo.min';
+
+import '../css/bootstrap.min.css';
+import '../css/custom.css';
+import '../css/fontawesome.css';
+import '../css/slick-theme.css';
+import '../css/templatemo.css';
+
+import * as bootstrap from 'bootstrap';
